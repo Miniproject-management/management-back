82bc8f4022f63d2551a6d71cab4c65da16e7a7ba
kimgeon <kimgeon0802@gmail.com>
2026-05-08 13:25:15 +0900

    healthcheckcontroller add

---
+++ src/main/java/com/mini3/backend/global/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.mini3.backend.global;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+// 프론트엔드(S3 등 다른 도메인)에서 오는 API 요청을 허용하기 위한 CORS 설정 (매우 중요)
+@CrossOrigin(origins = "*")
+public class healthCheckController {
+
+    @GetMapping("/health")
+    public String healthcheck() {
+        return "ok";
+    }
+}
