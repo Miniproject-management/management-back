60d8004fb0f42bdc74f58b7500185a9b6ad39e7c
kimgeon <kimgeon0802@gmail.com>
2026-05-11 17:12:46 +0900

    ATS main class block

--- src/main/java/com/mini3/backend/Mini3BackApplication.java
+++ src/main/java/com/mini3/backend/Mini3BackApplication.java
@@ -3,7 +3,11 @@ package com.mini3.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+        "com.mini3.backend"
+    }
+)
 public class Mini3BackApplication {
 
     public static void main(String[] args) {
