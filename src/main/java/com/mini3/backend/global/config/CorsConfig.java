d8929660a9138edc6866268fe5172f7d203d0549
Hyein <hi020514@yourdomain.com>
2026-05-12 09:54:28 +0900

    corsConfig 생성

---
+++ src/main/java/com/mini3/backend/global/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.mini3.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://k8s-miniproj-appingre-81c01d55c9-1855530779.ap-northeast-2.elb.amazonaws.com"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
